3441c67bfa0dd684a5abc48e2240891f681452f5
gibbsjoh <gibbsjoh@gmail.com>
2024-08-28 11:46:25 +0100

    Update htmlSnippets.py

--- htmlSnippets.py
+++ htmlSnippets.py
@@ -6,5 +6,5 @@
 # 
 #############################################################
 
-htmlHeader = '<!doctype html><html><head><meta charset="UTF-8"><title>fmPy2</title></head><body><img src="https://fmr-wb-p1.systems.private/img/VMO2.png" alt="company logo"/><h1>fmPy2</h1><p>'
+htmlHeader = '<!doctype html><html><head><meta charset="UTF-8"><title>fmPy2</title></head><body><img src="https://myserver.com/logo.png" alt="company logo"/><h1>fmPy2</h1><p>'
 htmlFooter = '</body></html>'
